337afabbd0315c033ba227c2fa387326b594f7cc
Marcelo Brito <marcelo.brito@virtus.ufcg.edu.br>
2019-02-18 09:23:13 -0300

    #master - FInishing the uncompact functionality

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const utils = require('./utils');
 utils.inputVerifications();
 
 if (utils.isCompactProcess() ) {
-    let folderToCompact = process.argv[3];
-    let zipFileNameToCreate = process.argv[4];
+    const folderToCompact = process.argv[3];
+    const zipFileNameToCreate = process.argv[4];
     
     let output = fs.createWriteStream(zipFileNameToCreate);
     let compactor = archiver(zipFileNameToCreate.split('.')[1]);
@@ -22,5 +22,10 @@ if (utils.isCompactProcess() ) {
 }
 
 if (utils.isUncompactProcess()) {
+    const zipFileNameToUncompact = process.argv[3];
+    const destinyOfFile = process.argv[4];
 
+    extract(zipFileNameToUncompact, {dir: __dirname + '/' + destinyOfFile}, function (err) {
+        console.error(err);
+    })
 }
